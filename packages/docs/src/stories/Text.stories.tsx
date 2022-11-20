6887b1e5bb06cd43bf0c7c4a653221db076db265
Eduardo Muniz <thirteenwannabegood@gmail.com>
2022-11-20 11:37:37 -0300

    Initial commit + core components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,43 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    children:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore tempora vitae quisquam reiciendis perspiciatis odit voluptatibus, vel voluptas aliquid fugit quo ad ducimus. Consequatur rem libero laboriosam culpa tenetur. Inventore?',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
