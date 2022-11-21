38d1163354d9d8996f7791f4270748028ba8280c
Eduardo Muniz <thirteenwannabegood@gmail.com>
2022-11-21 13:33:04 -0300

    storybook subpath correction try

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/ignite-ui/'
+      config.base = '/Ignite-UI/'
     }
 
     return config
